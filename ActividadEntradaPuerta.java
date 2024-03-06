12190a7cb4b50c4dcf1a1f64856e83c5406fbd10
Fernando <118523392+arroyofe@users.noreply.github.com>
2024-03-06 20:39:17 +0100

    Add files via upload
    Clase ActividadEntradaPuerta

---
+++ ActividadEntradaPuerta.java
@@ -0,0 +1,38 @@
+package p03.c03;
+
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Permite la antrada al parque
+ *
+ * @author Fernando Arroyo Redondo
+ * @author Francisco J Arroyo Redondo
+ * @version 1.0
+ * Práctica 3
+ */
+public class ActividadEntradaPuerta implements Runnable{
+	
+	
+	private static final int NUMENTRADAS=20;
+	private String idPuerta;
+	private Parque parque;
+	
+	public ActividadEntradaPuerta(String idPuerta, Parque parque) {
+		this.idPuerta=idPuerta;
+		this.parque=parque;
+	}
+	
+	public void run() {
+		for(int i=0;i<NUMENTRADAS;i++) {
+			try {
+				int numero = (int)(Math.random()*1000+100);
+				parque.entrarAlParque(idPuerta);
+				TimeUnit.MILLISECONDS.sleep(numero);
+			}catch (InterruptedException e) {
+				Logger.getGlobal().log(Level.INFO, "Entrada interrumpida en puerta: " + idPuerta);
+			}
+		}
+	}
+}
