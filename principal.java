1c51bb81b5d4b2de11303349ca52833c94cb753c
far0010 <93671310+far0010@users.noreply.github.com>
2024-03-14 17:15:19 +0100

    Add files via upload
    Clase con el main que inicia los hilos. Se puede dar una vuelta

---
+++ principal.java
@@ -0,0 +1,29 @@
+package p03.c03;
+
+
+public class principal {
+	/**
+	 * clase principal main()
+	 *
+	 * @author Fernando Arroyo Redondo
+	 * @author Francisco J Arroyo Redondo
+	 * @version 1.0
+	 * Práctica 3
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final int PUERTAS=5;
+		Parque parque = new Parque();
+		char letra_puerta = 'A'; //primera puerta
+		for (int i = 0; i < PUERTAS; i++) { //numero de puertas definido por parámetro
+			String puerta = ""+((char) (letra_puerta++));
+			// hilos de entrada
+			Thread entrada= new Thread (new ActividadEntradaPuerta(puerta, parque));	
+			// hilos de salida
+			Thread salida= new Thread (new ActividadSalidaPuerta(puerta, parque));
+			
+			entrada.start();
+			salida.start();
+		}
+	}
+}
