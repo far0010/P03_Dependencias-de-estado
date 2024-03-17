7060a5b5a62eabff01f2af7bae2e966d13e6497e
far0010 <93671310+far0010@users.noreply.github.com>
2024-03-17 13:17:43 +0100

    Update ActividadSalidaPuerta.java
    Añadidos algunos comentarios.

--- ActividadSalidaPuerta.java
+++ ActividadSalidaPuerta.java
@@ -14,16 +14,29 @@ import java.util.logging.Logger;
  */
 public class ActividadSalidaPuerta implements Runnable{
 	
+	/*
+	 * Número máximo de salidas en cad puerta.
+	 */
 	private static final int NUMSALIDAS=20;
+
+	/*
+	 * Identificación de la puerta utilizada.
+	 */
 	private String idPuerta;
+
+	/*
+	 * Parque que se usa.
+	 */
 	private Iparque parque;
-	
 	
 	public ActividadSalidaPuerta(String idPuerta, Iparque parque) {
 		this.idPuerta=idPuerta;
 		this.parque=parque;
 	}
-	
+
+	/**
+	 * Método run que lanza la actividad.
+	 */
 	public void run() {
 		for(int i=0;i<NUMSALIDAS;i++) {
 			try {
